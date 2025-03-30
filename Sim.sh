91837eec9958e02680c09d0d4634ff09f439cafa
hugomflavio <hmflavio92@gmail.com>
2025-03-30 17:32:46 -0400

    revert 'Sim06' entries to 'Sim' on main

---
+++ Sim.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+GAMEID=Sim
+GAMENAME=Sim
+GAMEDIR=$HOME/server/Sim
+PORT=5059
+
+. ../rc.subr.fc21
--- Sim06.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-GAMEID=Sim06
-GAMENAME=Sim06
-GAMEDIR=$HOME/server/Sim06
-PORT=5059
-
-. ../rc.subr.fc21
