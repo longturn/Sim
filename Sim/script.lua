2de20fc789bcc46f130549f2763949854155ac86
XHawk87 <hawk87@hotmail.co.uk>
2025-08-10 22:23:54 +0100

    Added resupply units #18

--- Sim/script.lua
+++ Sim/script.lua
@@ -24,3 +24,140 @@ end
 
 signal.connect("city_destroyed", "city_destroyed_callback")
 
+-------------------
+-- Unit resupply --
+-------------------
+
+-- Settings --
+
+-- When modifying this, also see `actionenabler_resupply` in `game.ruleset`.
+function is_supplier(unit)
+  return unit.utype:has_flag("Supplier")
+end
+
+-- When modifying this, also see `actionenabler_resupply` in `game.ruleset`.
+function can_resupply(unit)
+  return not unit.utype:has_flag("NonMil")
+end
+
+-- End of settings --
+
+-- How many hitpoints the unit needs.
+function unit_supplies_needed(unit)
+  return unit.utype.hp - unit:hp()
+end
+
+-- Find the supplier with the fewest supplies remaining on the given tile.
+function best_supplier(tile)
+  local best = nil
+  for unit in tile:units_iterate() do
+    if is_supplier(unit) then
+      if best == nil or best:hp() > unit:hp() then
+        best = unit
+      end
+    end
+  end
+  return best
+end
+
+-- Check if the tile contains only supply units
+function tile_has_only_suppliers(tile)
+  for unit in tile:units_iterate() do
+    if not is_supplier(unit) then
+      return false
+    end
+  end
+  return true
+end
+
+-- How many hitpoints all suppliable units on the tile need.
+function tile_supplies_needed(tile)
+  local total = 0
+  for unit in tile:units_iterate() do
+    if can_resupply(unit) and not is_supplier(unit) then
+      total = total + unit_supplies_needed(unit)
+    end
+  end
+  return total
+end
+
+-- Transfer supplies from the supplier to the unit up to the given amount.
+function resupply_by_amount(unit, supplier, amount)
+  local heal = math.min(amount, supplier:hp(), unit_supplies_needed(unit))
+  if heal <= 0 then return end -- No resupply needed
+  unit:recover_hp(heal)
+  supplier:damage_hp(heal, "used")
+end
+
+-- Transfer all supplies possible from the supplier to the unit.
+function resupply_unit(unit, supplier)
+  resupply_by_amount(supplier:hp())
+end
+
+-- Transfer all supplies possible from the supplier to all suppliable units on
+-- the tile, in round-robin order.
+function resupply_tile(supplier)
+  while tile_supplies_needed(supplier.tile) > 0 do
+    for unit in supplier.tile:units_iterate() do
+      if can_resupply(unit) and not is_supplier(unit) then
+        resupply_by_amount(unit, supplier, 1)
+        if not supplier:exists() then 
+          return -- Supply unit is exhausted
+        end 
+      end
+    end
+  end
+end
+
+-- Unit action to resupply at the targetted supply unit.
+function resupply_action_handler(action, actor, target)
+  if action:rule_name() ~= "User Action 1" then return end -- Resupply
+  resupply_unit(actor, target)
+end
+signal.connect("action_started_unit_unit", "resupply_action_handler")
+
+-- Automatic resupply at turn start
+function auto_resupply_tile(tile)
+  if tile:num_units() <= 0 then return end -- Skip empty tiles
+  local supplier = best_supplier(tile)
+  while supplier and supplier:exists() do
+    resupply_tile(supplier)
+    if supplier:exists() then
+      return -- All units must have been fully supplied if this still exists
+    else
+      supplier = best_supplier(tile) -- If exhausted, get the next best supplier
+    end
+  end
+end
+
+function auto_resupply_phase()
+  for tile in whole_map_iterate() do
+    auto_resupply_tile(tile)
+  end
+end
+signal.connect("turn_begin", "auto_resupply_phase")
+
+-- Attacking supply units in cities
+function citizens_mount_defence(city, attacker)
+  -- TODO: Calculate chance of citizen defence
+  return false
+end
+
+function attackers_steal_supplies(attacker, city)
+  -- TODO: On fail, calculate stolen supplies
+end
+
+function supply_attacked_in_city_handler(action, actor, target)
+  if action ~= "Attack" then return end
+  local city = target:city()
+  if not city then return end
+  if not tile_has_only_suppliers(target) then return end
+  if citizens_mount_defence(city, actor) then return end
+  attackers_steal_supplies(actor, city)
+end
+signal.connect("action_started_unit_units", "supply_attacked_in_city_handler")
+
+--------------------------
+-- End of Unit Resupply --
+--------------------------
+
